278c736435ddbb302688a19bf1ea96f193b39129
simon <bonnefoy.simon@protonmail.com>
2019-12-03 14:00:51 +0100

    added search for emails

--- crawler.py
+++ crawler.py
@@ -2,36 +2,68 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
-
+import time
 import re
 
-
-def is_link_from_domain(link):
-    '''
-    :param link:
-    :return bool:
-    function that checks if the link found belongs to the domain of the target url.
-    retrun true is the link belongs to the same domain.
-    '''
 
 def join_url(link):
     return urljoin(target_url, link)
 
 target_url = 'http://192.168.1.10/mutillidae/'
+#target_url = 'https://pharmacieagroparc.com/'
+target_links = []
+target_mails = []
+def crawl(url):
+    print('Crawling through %s'%url)
+    response = requests.get(url)
 
-response = requests.get(target_url)
-#print(response.text)
-links = re.findall('(?:href=")(.*?)"',response.content.decode())
-#print(links)
-for url in links:
-    if url and '#' not in url:
-        url = join_url(url)
-        print(url)
-        if target_url in url:
-            print('URL belongs to the target domain!')
+    #retrieve all the links, from the href tag
+    try:
+        links = re.findall('(?:href=")(.*?)"',response.content.decode('cp1252'))
+        mails = re.findall('[\w_.\-]*@\w*.\w*',response.content.decode('cp1252'))
+        for mail in mails:
+            if mail not in target_mails:
+                target_mails.append(mail)
+
+        #regexp to search for mail. Match alphanum -_. chars @ alphanum.alphanum
+
+        #loop over the links found in the page
+        for link in links:
+
+            #check if the link is not null
+            if link and '#' not in link:
+
+                #make sure the link is absolute
+                link = join_url(link)
+                #print(link)
+
+                #Check if the link belongs to the domain
+                if target_url in link and link not in target_links:
+
+                    #for now, forget about the files
+                    if '.ico' in link or '.pdf' in link:
+                        continue
+
+                    #add link to the list
+                    target_links.append(link)
+              #      print(url)
+
+                    #Crawl the new link
+                    crawl(link)
+    except:
+        pass
+
+
+
+if __name__ == '__main__':
+    crawl(target_url)
+    print('Links found \n')
+    for link in target_links:
+        print(link)
+    print('Mails found \n')
+    for mail in target_mails :
+        print(mail)
 
 
 
 
-#soup = bs(response.text, 'html.parser')
-#print(soup.prettify())
