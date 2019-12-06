adb5bfc7ed9f3847cf4b304815ff36ab4e1a4ff4
simon <bonnefoy.simon@protonmail.com>
2019-12-06 18:40:00 +0100

    update web_fuzzer

--- ozyptila/web_fuzzer.py
+++ ozyptila/web_fuzzer.py
@@ -39,38 +39,55 @@ class WebFuzzer():
         #    t.start()
         #    t.join()
         #Until queue is not empty, keep loopin on urls
+        #while not self.target_url_q.empty():
+            #self.fuzz(self.target_url_q.get().rstrip())
+
+        #while not self.target_url_q.empty():
+        #    pool = Pool(2)
+        #    asyncresponse = pool.map(self.fuzz(self.target_url_q.get().rstrip()))
+        #    pool.close()
+        #    pool.join()
+
         while not self.target_url_q.empty():
-            self.fuzz(self.target_url_q.get().rstrip())
-            #pool = Pool(2)
-            #asyncresponse = pool.map(self.fuzz(self.target_url_q.get().rstrip()))
-            #pool.close()
-            #pool.join()
+            for i in range(10):
+                t = threading.Thread(target = self.fuzz(self.target_url_q.get().rstrip()))
+                t.start()
+                t.join()
+
+        #    pool.close()
+        #    pool.join()
 
     def fuzz(self, url):
         '''method that fuzzes the target web site'''
-        #Looping over all the file names in the dictionnary
-        #until queue is empty
-        print(url)
-        #url_list_q = copy.copy(self.wordlist_q)
-
         #We need to recreate the queue everty time
         #since the get method remove items
-        url_list_q = self.build_wordlist(self.wordlist)
-        while not url_list_q.empty():
-            file = url_list_q.get().rstrip()
+        file_list_q = self.build_wordlist(self.wordlist)
+
+        #Looping over all the file names in the dictionnary
+        #until queue is empty
+        while not file_list_q.empty():
+            file = file_list_q.get().rstrip()
 
             #Check if url is / ended
             if  url[-1] != '/': url += '/'
 
             #creating the url to check
             link = url + str(file.decode())
-            print(link)
+            #print(link)
 
             #Getting the link
             response = requests.get(link)
 
             #retrieving code response
-            print(link, response.status_code)
+            if response.status_code != 404:
+                print(link, response.status_code)
+
+                # add link to list of links found
+                if link not in self.target_files:
+                    self.target_files.append(link)
+
+                    #Add link the url queue to be fuzzed in some further rounds
+                    self.target_url_q.put(link)
 
     def add_known_links(self, known_links):
         '''This method gives all the link already know from
@@ -90,7 +107,7 @@ class WebFuzzer():
 
 
     def build_wordlist(self, wordlist_file):
-        '''Method to set the dictionnary used to brute force
+        '''Method to set the dictionnary used to fuzz
         directories in a Queue object for multi-threading'''
 
         print("Building the word list")
@@ -104,14 +121,4 @@ class WebFuzzer():
         for word in raw_words:
             word = word.rstrip()
             words.put(word)
-            #if self.resume is not None:
-            #    if found_resume:
-            #        words.put(word)
-            #    else:
-            #        if word == self.resume:
-            #            found_resume = True
-            #            print("Resuming wordlist from: %s" % self.resume)
-            #else:
-            #    words.put(word)
-
         return words
