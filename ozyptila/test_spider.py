9b23fce9fb4d673a451685394e71cfa429beff4b
simon <bonnefoy.simon@protonmail.com>
2019-12-27 21:31:38 +0100

    updÂ

---
+++ ozyptila/test_spider.py
@@ -0,0 +1,21 @@
+import pytest
+from web_crawler import WebCrawler
+from web_fuzzer import WebFuzzer
+from subdomain_fuzzer import SubDomainFuzzer
+
+
+
+def test_spider():
+    crawler = WebCrawler('https://example.com/', '', 1)
+
+    # Start crawler
+    assert None == crawler.run()
+
+def test_web_fuzzer():
+    folders_fuzzer = WebFuzzer('https://example.com', './unit_test/test_wordlist.txt', 0)
+    assert None == folders_fuzzer.run(4)
+
+def test_subdomain_fuzzer():
+    subdomain_fuzz = SubDomainFuzzer('htps://example.com', './unit_test/test_wordlist.txt', 0)
+    assert None == subdomain_fuzz.run(2)
+
